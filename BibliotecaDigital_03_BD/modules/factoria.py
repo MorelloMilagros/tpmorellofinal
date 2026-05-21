c8dbcc65c8913158ab2fc495fb9d661a4cb8295a
MorelloMilagros <morellodeppelerm@gmail.com>
2026-05-21 16:22:41 -0300

    Delete BibliotecaDigital_03_BD directory

--- BibliotecaDigital_03_BD/modules/factoria.py
+++
@@ -1,6 +0,0 @@
-from modules.repositorio_concreto import RepositorioSQLAlchemy
-from modules.config import crear_engine
-
-def crear_repositorio():
-    Session = crear_engine()
-    return RepositorioSQLAlchemy(Session())
